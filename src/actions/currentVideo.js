85991ee0dd985e74b9031ceefd364a3f2e513f6b
Anthony Sim <anthonyysim@gmail.com>
2020-12-07 20:19:18 -0800

    getting started

--- src/actions/currentVideo.js
+++ src/actions/currentVideo.js
@@ -1,5 +1,22 @@
-var changeVideo = (video) => ({
-  //TODO:  Return some action object to change the currently playing video.
-});
+// var changeVideo = (video) => ({
+//   //TODO:  Return some action object to change the currently playing video.
 
-export default changeVideo;
+// });
+
+// export default changeVideo;
+
+
+  export function changeVideo(video) {
+  return {
+    type: 'CHANGE_VIDEO',
+    video
+  }
+  }
+
+
+// export function itemsHasErrored(bool) {
+//   return {
+//       type: 'ITEMS_HAS_ERRORED',
+//       hasErrored: bool
+//   };
+// }
