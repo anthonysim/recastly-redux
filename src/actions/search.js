7b92a723e14dbf0767bca5d1a64023be3494184e
Anthony Sim <anthonyysim@gmail.com>
2020-12-08 18:40:13 -0800

    Complete defining action creators

--- src/actions/search.js
+++ src/actions/search.js
@@ -3,7 +3,7 @@ import changeVideoList from './videoList.js';
 import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
 
-
+// api call
 var handleVideoSearch = (q) => {
   return (dispatch) => {
     searchYouTube({key: YOUTUBE_API_KEY, query: q, max: 5}, (data) => {
