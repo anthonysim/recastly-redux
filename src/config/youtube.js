586ded82984c73b0e1a6811c7ecfd9f00f65b6b4
Anthony Sim <anthonyysim@gmail.com>
2020-12-08 18:39:15 -0800

    Complete planning state

---
+++ src/config/youtube.js
@@ -0,0 +1,5 @@
+// Put your YouTube API keys here!
+// remove "a" I added it in there to prevent the key from dying from multiple api calls
+var YOUTUBE_API_KEY = 'AIzaSyBI5ZqZaqGO-fw5l5H4Vmjh59BgGckqXSI';
+
+export default YOUTUBE_API_KEY;
