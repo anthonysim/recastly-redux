5f9c5c6f14b758c5e3c57c516b3c71cbb993b099
Anthony Sim <anthonyysim@gmail.com>
2020-12-09 19:11:56 -0800

    Complete replacing components with containers

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -1,8 +1,6 @@
 import { connect } from 'react-redux';
 import Search from './../components/Search.js';
-import handleSearchChange from '../actions/search.js';
-
-var SearchContainer = () => {};
+import handleVideoSearch from '../actions/search.js';
 
 //TODO: define a SearchContainer component which will hook up your action
 // dispatchers with your search component props.
@@ -10,4 +8,21 @@ var SearchContainer = () => {};
 //state and dispatch mappings.
 
 
+
+
+const mapStateToProps = (state) => {
+  return {
+    state
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleSearchInputChange: (query) => {
+      dispatch(handleVideoSearch(query));
+    }
+  };
+};
+
+let SearchContainer = connect(mapStateToProps, mapDispatchToProps)(Search);
 export default SearchContainer;
